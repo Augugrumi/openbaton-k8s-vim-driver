6f3bfece7aa8528cb5ee2239d36a04b2148451e0
mzanella <zanna0150@gmail.com>
2018-08-22 09:38:24 +0200

    Change implementation of K8sVimInstance

--- src/main/java/org/augugrumi/k8svimdriver/K8sVimInstance.java
+++ src/main/java/org/augugrumi/k8svimdriver/K8sVimInstance.java
@@ -21,18 +21,63 @@ import java.net.URL;
 import java.util.*;
 import java.util.logging.Logger;
 
-public class K8sVimInstance extends GenericVimInstance {
+public class K8sVimInstance extends BaseVimInstance {
 
     private static final Logger LOGGER = Logger.getLogger(K8sVimInstance.class.getName());
 
     private String address = System.getenv("HARBOR_ADDRESS") == null ? "0.0.0.0" : System.getenv("HARBOR_ADDRESS") + HarborConstants.VNF;
+
+    @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    private Set<K8sImage> images;
 
     K8sVimInstance () {
         super();
         LOGGER.info("Harbor address: " + address);
     }
 
-    String getAddress() {
+    @Override
+    public Set<? extends BaseNfvImage> getImages() {
+        return images;
+    }
+
+    @Override
+    public Set<? extends BaseNetwork> getNetworks() {
+        return new HashSet<>();
+    }
+
+    @Override
+    public void addAllNetworks(Collection<BaseNetwork> networks) {
+
+    }
+
+    @Override
+    public void addAllImages(Collection<BaseNfvImage> images) {
+        if (this.images == null)
+            this.images = new HashSet<>();
+        images.forEach(i -> this.images.add((K8sImage) i));
+    }
+
+    @Override
+    public void removeAllNetworks(Collection<BaseNetwork> networks) {
+
+    }
+
+    @Override
+    public void removeAllImages(Collection<BaseNfvImage> images) {
+        this.images.clear();
+    }
+
+    @Override
+    public void addImage(BaseNfvImage image) {
+
+    }
+
+    @Override
+    public void addNetwork(BaseNetwork network) {
+
+    }
+
+    public String getAddress() {
         return address;
     }
 
